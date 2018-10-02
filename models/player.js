9ccc8b749a7dd3c4c8723cae87416a4bd9fd3c7c
Trizzlenova <tdswayzee@gmail.com>
2018-10-02 14:35:20 -0700

    working on controllers/models

--- models/player.js
+++ models/player.js
@@ -5,5 +5,19 @@ let PlayerSchema = new Schema({
   name: String,
   team: String,
   position: String,
-
+  number: Number,
+  week: Number,
+  passingYds: Number,
+  passingTD: Number,
+  passingAtt: Number,
+  passingComp: Number,
+  interceptions: Number,
+  rushingYds: Number,
+  rushingTD: Number,
+  fumbles: Number,
+  fumblesLost: Number
 })
+
+let Player = mongoose.model('Player', PlayerSchema);
+
+module.exports = Player;
